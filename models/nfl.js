465518fee47f4d8d6c923ced4c58bf1ad64327ce
gkourouma <gkourouma2@gmail.com>
2025-03-07 05:55:17 -0600

    Created form pages and post method for info entered in by user.

--- models/nfl.js
+++ models/nfl.js
@@ -1,10 +1,16 @@
+//Requiring Mongoose
 const mongoose = require("mongoose");
 
+//Creating nfl schema
 const nflSchema = new mongoose.Schema({
   team: String,
+  conference: String,
+  division: String,
   isPlayoffContender: Boolean,
 });
 
+//Connecting schema to a model
 const Nfl = mongoose.model("Nfl", nflSchema);
 
-module.export = Nfl;
+//Exporting the model
+module.exports = Nfl;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
+const Nfl = require("./models/nfl.js");
 
 dotenv.config();
 const app = express();
@@ -11,9 +12,30 @@ mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
-//Build Route
+//middleware
+app.use(express.urlencoded({ extended: false }));
+
+//Build Route... This leads to Home Page
 app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+//Takes us to our form page
+app.get("/teams/new", (req, res) => {
+  res.render("teams/new.ejs");
+});
+
+// This post the information entered by the user into our database
+app.post("/teams", async (req, res) => {
+  if (req.body.isPlayoffContender === "on") {
+    req.body.isPlayoffContender = true;
+  } else {
+    req.body.isPlayoffContender = false;
+  }
+
+  await Nfl.create(req.body);
+  console.log(req.body);
+  res.redirect("/teams/new");
 });
 
 app.listen(3000, () => {
